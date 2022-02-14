417941891ef1a1e2012debc3f9af43780d02ba62
kamikigit <kamiki0906@gmail.com>
2022-02-14 21:56:05 +0900

    made branch

--- node/app/react-sample/src/App.js
+++ node/app/react-sample/src/App.js
@@ -1,27 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [small, setSmall] = useState(false);
+  useEffect(() =>{
+    if (typeof window !=="undefined"){
+      window.addEventListener("scroll", () => 
+        setSmall(window.pageYOffset > 10)
+      );
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          helloworld
-        </p>
-      </header>
+    <div className="wrapper">
+    <main className="content">
+      {/* header */}
+      {small ? <HeaderSmall /> : <HeaderBig />}
+      {/*  /header  */}
+
+    {/* <!-- gnav --> */}
+    <nav className="gnav">
+      <div className="gnav-inner">
+        <ul className="gnav-list">
+          <li className="gnav-item"><a href="#about">about</a></li>
+          <li className="gnav-item"><a href="#works">works</a></li>
+        </ul>
+      </div>
+    </nav>
+    {/* <!-- /gnav --> */}
+
+    {/* <!-- about --> */}
+    <div id="about">
+      <p>
+        <br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/>
+
+
+      </p>
     </div>
+    {/* <!-- /about--> */}
+
+    {/* works */}
+    <div id ="works">
+      </div>
+    {/* /works */}
+
+  {/* <!-- footer --> */}
+  <footer className="footer">
+    <div className="copyright">&copy;Kamiki</div>
+  </footer>
+  {/* <!-- /footer --> */}
+
+    </main>
+  </div>
+  );
+}
+
+function HeaderBig(){
+  return (
+    <header className = "header big">
+      <div className="header-inner">
+        <p className="header-title font-effect-anaglyph">Kamiki</p>
+        <p className="header-subtitle ">Hello World :)</p>
+      </div>
+    </header>
+  );
+}
+
+function HeaderSmall(){
+  return (
+    <header className = "small">
+      <div className="header-inner">
+        <p className="header-title font-effect-anaglyph small">Kamiki</p>
+        <p className="header-subtitle small">Hello World :)</p>
+      </div>
+    </header>
   );
 }
 
