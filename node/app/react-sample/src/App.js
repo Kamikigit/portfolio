47c9392cd9f2f56a4678a816470b82c38aeeb8bf
kamikigit <kamiki0906@gmail.com>
2022-02-15 01:00:11 +0900

    made Link section

--- node/app/react-sample/src/App.js
+++ node/app/react-sample/src/App.js
@@ -21,7 +21,7 @@ function App() {
     <nav className="gnav">
       <div className="gnav-inner">
         <ul className="gnav-list">
-          <li className="gnav-item"><a href="#about">about</a></li>
+          <li className="gnav-item"><a href="#links">links</a></li>
           <li className="gnav-item"><a href="#works">works</a></li>
         </ul>
       </div>
@@ -29,25 +29,41 @@ function App() {
     {/* <!-- /gnav --> */}
 
     {/* <!-- about --> */}
-    <div id="about">
-      <p>
-        <br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/>
+    <section id="links">
+      <div class="inner">
+      <h2>Links</h2>
+        <div class="box">
+          <p>twitter</p>
+          <a href="https://twitter.com/kam_twi"></a>
+        </div>
 
+        <div class="box">
+          <p>はてなブログ</p>
+          <a href="https://kam.hatenadiary.com"></a>
+        </div>
 
-      </p>
-    </div>
+        <div class="box">
+          <p>GitHub</p>
+          <a href="https://github.com/kamikigit"></a>
+        </div>
+
+        <div class="box">
+          <p>Qiita</p>
+          <a href="https://qiita.com/kam_qiita"></a>
+        </div>
+
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      </div>
+    </section>
     {/* <!-- /about--> */}
 
     {/* works */}
@@ -70,7 +86,7 @@ function HeaderBig(){
   return (
     <header className = "header big">
       <div className="header-inner">
-        <p className="header-title font-effect-anaglyph">Kamiki</p>
+        <p className="header-title font-effect-anaglyph">Kamiki's Portfolio</p>
         <p className="header-subtitle ">Hello World :)</p>
       </div>
     </header>
